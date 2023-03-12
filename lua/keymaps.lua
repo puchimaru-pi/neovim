5abdcf789e50c5347ba6f894420b5c78bea7eaf9
puchimaru-pi <puchi.maru.milk@gmail.com>
2023-03-12 13:16:12 +0900

    vim-cmpをいれてみたかった

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,11 +7,5 @@ local keymap = vim.api.nvim_set_keymap
 --ESC*2 nohlsearch
 keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 
---insert mode move
-keymap("i", "<C-h>", "<Left>", opts)
-keymap("i", "<C-j>", "<Down>", opts)
-keymap("i", "<C-k>", "<Up>", opts)
-keymap("i", "<C-l>", "<Right>", opts)
-
 keymap('n', 'j', 'gj', { noremap = true })
 keymap('n', 'k', 'gk', { noremap = true })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,20 +11,62 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
+--んなー
 require("lazy").setup({
+    --colorscheme
     {
         'rose-pine/neovim',
         name = 'rose-pine'
     },
+
+    --statusline
     {
         'nvim-lualine/lualine.nvim',
+        event = 'VimEnter',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-        require('lualine').setup()
+            --require('lualine').setup()
+            require('pluginconfig/lualine')
         end
     },
+
+    --auto completion
     {
-        'nvim-telescope/terescope.nvim', tag = '0.1.1',
-        dependenciens = { 'nvim-lua/plenary.nvim' }
+        'hrsh7th/nvim-cmp',
+        event = 'VimEnter',
+        config = function()
+            require('pluginconfig/nvim-cmp')
+        end,
+        dependencies = {
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-emoji' },
+            { 'hrsh7th/cmp-calc' },
+            { 'uga-rosa/cmp-dictionary' },
+            { 'hrsh7th/cmp-cmdline' },
+            { 
+                'onsails/lspkind-nvim',
+                config = function()
+                    require('pluginconfig/lspkind-nvim')
+                end,
+            },  
+        },
+    },
+
+    --telescope
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        event = 'VimEnter',
+        dependencies = {
+            { 'nvim-lua/plenary.nvim'},
+            {
+                'nvim-telescope/telescope-frecency.nvim',
+                config = function()
+                    require('telescope').load_extension('frecency')
+                end,
+                dependencies = { 'kkharji/sqlite.lua' },
+            },
+        },
     },
 })
